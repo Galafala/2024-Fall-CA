ab0699d4f4bc3a182267f00100903961b2f22b32
Galafala <ben100719@gmail.com>
2024-10-08 20:56:00 +0800

    Feat: Add fp32 library

--- Assignment1/C-code/main.c
+++ Assignment1/C-code/main.c
@@ -4,46 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "bfp16_utils.h"
+#include "fp32_utils.h"
 #include "print_utils.h"
 
-int seed(bf16 x, bf16 n, bf16 e) {
-  n.s = 0x8000;
-  n.bits &= 0x7FFF;
-  n.bits |= 0x8000;
-  bf16 added = bf16add(bf16square(x), n);
-  added.s = 0;
-  added.bits &= 0x7FFF;
-  return bf16cmp(added, e);
-}
-
 int main() {
-  float num = 88;
-  float error = 0.5;
+  float num = 49;
+  float error = 0.01;
 
-  bf16 n = fp32_to_bf16(*(uint32_t*)&num);
+  float half = fdiv2(num);
 
-  bf16 x = bf16div2(n);
-
-  bf16 e = fp32_to_bf16(*(uint32_t*)&error);
-
-  // bf16 y = bf16square(x);
-
-  // float k = bf16_to_fp32(y);
-  // printf("Square: %f\n", k);
-  // fprint(num * num * 0.25);
-
-  while (seed(x, n, e) == 1) {
-    bf16 temp = bf16div(n, x);
-    x = bf16add(x, temp);
-    x = bf16div2(x);
+  while (fcmp(fsquare(half), num) == 1) {
+    float diff = fdiv(num, half);
+    diff = fadd(half, diff);
+    half = fdiv2(half);
   }
 
-  float a = bf16_to_fp32(x);
-
-  printf("[Important] Square root: %f\n", a);
-  printf("Is perfect square: %s\n", bf16cmp(bf16square(x), n) == 0 ? "true" : "false");
-  printf("Square root by built-in function: %f\n", sqrt(num));
+  printf("The square root of %f is %f\n", num, half);
 
   return 0;
 }
