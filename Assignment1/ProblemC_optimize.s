d19f0c2ef94d7ef63f612aeb198798799f1cdedb
Galafala <ben100719@gmail.com>
2024-10-11 22:06:07 +0800

    Format: Format the ProblemC_optimize.s

--- Assignment1/ProblemC_optimize.s
+++ Assignment1/ProblemC_optimize.s
@@ -7,20 +7,19 @@ str3: .asciz "\nThe number of "
 .globl main
 
 main:
-    li t0, 0xc2440000
-    sw t0, 0(s0)
-    li t0, 0x001f0456
-    sw t0, 4(s0)
-    li t0, 0x80000000
-    sw t0, 8(s0)
-    li t0, 0x00000001
-    sw t0, 12(s0)
-    li t0, 0x8cc0ffff
-    sw t0, 16(s0)
-    li t0, 0x00f00001
-    sw t0, 20(s0)
-
-    li s1, 6
+  li t0, 0xc2440000
+  sw t0, 0(s0)
+  li t0, 0x001f0456
+  sw t0, 4(s0)
+  li t0, 0x80000000
+  sw t0, 8(s0)
+  li t0, 0x00000001
+  sw t0, 12(s0)
+  li t0, 0x8cc0ffff
+  sw t0, 16(s0)
+  li t0, 0x00f00001
+  sw t0, 20(s0)
+  li s1, 6
   
 main_loop:
     # call function part
@@ -28,55 +27,45 @@ main_loop:
     addi sp, sp, -4
     jal ra, clz32
     jal ra, print
-    addi sp, sp, 4
-    
+    addi sp, sp, 4    
     addi s0, s0, 4 # a[i+1]
     addi s1, s1, -1 # s1 -= 1
-    bnez s1, main_loop # if s1 != 0, loop the next test data
-    
+    bnez s1, main_loop # if s1 != 0, loop the next test data    
     # exit
     li a7, 93     
     ecall
     
 clz32:
-    li t0, 0 # r = 0
-    mv t3, a3 # t3 = a3
-    
-    li t2, 0x00010000
-    sltu t1, t3, t2 # c = (x < 0x00010000) ? 1 : 0
-    slli t1, t1, 4 # c = (x < 0x00010000) << 4
-    add t0, t0, t1 # r += c
-    sll t3, t3, t1 # x <<= c
-    
-    li t2, 0x01000000
-    sltu t1, t3, t2 # c = (x < 0x01000000) ? 1 : 0
-    slli t1, t1, 3 # c = (x < 0x01000000) << 3
-    add t0, t0, t1 # r += c
-    sll t3, t3, t1 # x <<= c
-    
-    li t2, 0x10000000
-    sltu t1, t3, t2 # c = (x < 0x01000000) ? 1 : 0
-    slli t1, t1, 2 # c = (x < 0x01000000) << 2
-    add t0, t0, t1 # r += c
-    sll t3, t3, t1 # x <<= c
-    
-    li t2, 0x40000000
-    sltu t1, t3, t2 # c = (x < 0x01000000) ? 1 : 0
-    slli t1, t1, 1 # c = (x < 0x01000000) << 1
-    add t0, t0, t1 # r += c
-    sll t3, t3, t1 # x <<= c
-    
-    li t2, 0x80000000
-    sltu t1, t3, t2 # c = (x < 0x01000000) ? 1 : 0
-    add t0, t0, t1 # r += c
-    sll t3, t3, t1 # x <<= c
-    
-    sltiu t1, t3, 1
-    add t0, t0, t1
-    
-    sw t0, 0(sp)
-
-    jalr zero, ra, 0
+  li t0, 0 # r = 0
+  mv t3, a3 # t3 = a3  
+  li t2, 0x00010000
+  sltu t1, t3, t2 # c = (x < 0x00010000) ? 1 : 0
+  slli t1, t1, 4 # c = (x < 0x00010000) << 4
+  add t0, t0, t1 # r += c
+  sll t3, t3, t1 # x <<= c  
+  li t2, 0x01000000
+  sltu t1, t3, t2 # c = (x < 0x01000000) ? 1 : 0
+  slli t1, t1, 3 # c = (x < 0x01000000) << 3
+  add t0, t0, t1 # r += c
+  sll t3, t3, t1 # x <<= c  
+  li t2, 0x10000000
+  sltu t1, t3, t2 # c = (x < 0x01000000) ? 1 : 0
+  slli t1, t1, 2 # c = (x < 0x01000000) << 2
+  add t0, t0, t1 # r += c
+  sll t3, t3, t1 # x <<= c  
+  li t2, 0x40000000
+  sltu t1, t3, t2 # c = (x < 0x01000000) ? 1 : 0
+  slli t1, t1, 1 # c = (x < 0x01000000) << 1
+  add t0, t0, t1 # r += c
+  sll t3, t3, t1 # x <<= c  
+  li t2, 0x80000000
+  sltu t1, t3, t2 # c = (x < 0x01000000) ? 1 : 0
+  add t0, t0, t1 # r += c
+  sll t3, t3, t1 # x <<= c  
+  sltiu t1, t3, 1
+  add t0, t0, t1  
+  sw t0, 0(sp)
+  jalr zero, ra, 0
 
 print:
     la a0, str3 # "\nThe number of "
