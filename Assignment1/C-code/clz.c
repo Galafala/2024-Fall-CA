e5d0206b7522ff6869307a4f191d7623dcffc9ce
Tzu-Ching Kuo <99483761+Galafala@users.noreply.github.com>
2024-10-14 22:55:32 +0800

    Refactor/directory (#12)
    * Refactor: Create others directory
    
    - Move floating-point to others
    
    * Refactor: Clean code
    
    * Rename: Rename clz32 and clz to clz_opt and clz_org respectively

--- Assignment1/C-code/clz.c
+++ Assignment1/C-code/clz.c
@@ -1,5 +1,3 @@
-#include <stdint.h>
-
 #include "stdint.h"
 #include "stdio.h"
 
@@ -13,23 +11,18 @@ void printBinary32(uint32_t n) {
   printf("\n");
 }
 
-uint32_t myclz(uint32_t x) {
-  int r = !x, c;
-  c = (x < 0x00010000) << 4;
-  r += c;
-  x <<= c;  // off 16
-  c = (x < 0x01000000) << 3;
-  r += c;
-  x <<= c;  // off 8
-  c = (x < 0x10000000) << 2;
-  r += c;
-  x <<= c;  // off 4
-  c = (x >> (32 - 4 - 1)) & 0x1e;
-  r += (0x55af >> c) & 3;
-  return r;
+// orignial clz version
+static inline int clz_org(uint32_t x) {
+  int count = 0;
+  for (int i = 31; i >= 0; --i) {
+    if (x & (1U << i)) break;
+    count++;
+  }
+  return count;
 }
 
-int clz32(uint32_t x) {
+// optimized clz version
+static inline int clz_opt(uint32_t x) {
   int r = 0, c;
   c = (x < 0x00010000) << 4;
   r += c;
@@ -52,7 +45,7 @@ int clz32(uint32_t x) {
 
 int main() {
   uint32_t x = 0x00001;
-  uint32_t r = clz32(x);
+  uint32_t r = clz_opt(x);
   printf("r: %d\n", r);
   printBinary32(x);
   return 0;
